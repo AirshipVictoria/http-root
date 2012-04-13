8ff9224200caab9843a0801c18d8f9cef1786bab
Matt Stephenson <mattstep@mattstep.net>
2012-04-12 17:52:22 -0700

    initial gh-pages commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(".menu ul li").click(function () {
+    $(".content").fadeTo(0,0);
+    var el = $(this);
+    var c = $("#" + el.attr("content"));
+    $(".content .box").each(function() {
+       $(this).hide();
+    });
+    c.show();
+    $(".content").fadeTo(500,1);
+});
+
+$(document).ready(function() {
+    $(".content").fadeTo(0, 1);
+});
+
+$("#crew img").click(function() {
+    var id = $(this).attr("id");
+    $(".bio .boxHeader").html(CrewData[id].name);
+    $(".bio #biocontent").html(CrewData[id].bio);
+    $(".bio").fadeTo(500, 1);
+});
+
+$(".menu ul li").mouseover(function () {
+    $(this).animate({color:"#a08050"},300);
+});
+
+$(".menu ul li").mouseout(function () {
+    $(this).animate({color:"#ddd"},100);
+});
+
+$("#history p").each(function() {
+    $(this).html(avHistory[$(this).attr("id")]);
+});
